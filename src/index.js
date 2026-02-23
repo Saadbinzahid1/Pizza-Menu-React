939ab18a0018e07370c85ad811f435042abc904e
Saadbinzahid1 <sbinzahid2@gmail.com>
2026-02-23 16:43:10 +0500

    Added the props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,32 +58,51 @@ function App() {
 }
 
 function Header() {
-  return <h1>Times Pizza</h1>;
+  return (
+    <header className="header">
+      <h1>Times Pizza</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <div className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={12}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={10}
+      />
+    </div>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}$</span>
+      </div>
     </div>
   );
 }
 
 function Footer() {
-  return <p>{new Date().toLocaleTimeString()}. We are open</p>;
-}
-
-function Pizza() {
   return (
-    <div>
-      {" "}
-      <img src="pizzas/salamino.jpg" alt="Pizza Salamino" />
-      <h2>Pizza Salamino</h2>
-      <p>Tomato, mozarella, and pepperoni</p>
-    </div>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We are open
+    </footer>
   );
 }
 
